4f361ea7868ced18dee772fbbd3a5993858d0cd3
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-08-21 05:53:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+
+def init_db():
+    with sqlite3.connect('database.db') as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS xss (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                injection TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+
+@app.route('/', methods=['GET'])
+@app.route('/<_>', methods=['GET'])
+def home(_=None):
+    try:
+        confessions = sqlite3.connect("database.db")
+        cursor = confessions.cursor() 
+        cursor.execute('SELECT * FROM xss') 
+        data = cursor.fetchall()
+        confessions.commit()
+    except sqlite3.Error as e:
+        print(f"\033[91mAn error occurred:\033[00m {e} ")
+        init_db()
+    finally:
+        if confessions:
+            confessions.close()
+    return render_template('index.html', data=data)
+
+
+@app.route('/inject', methods=['GET', 'POST'])
+def Post_Confession():
+    if request.method == 'POST': 
+        injection = request.form['injection'] 
+        try:
+            confessions = sqlite3.connect("database.db")
+            cursor = confessions.cursor()
+            cursor.execute("INSERT INTO xss (injection) VALUES (?)", (injection, ) )
+            confessions.commit()
+        except sqlite3.Error as e:
+            print(f"\033[91mAn error occurred:\033[00m {e} ")
+            init_db()
+        finally:
+            if confessions:
+                confessions.close()
+        return redirect("/", code=302)
+    else:
+        return render_template('inject.html')
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host="0.0.0.0", port=80, debug=True)
