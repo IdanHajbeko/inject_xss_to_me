64185e5975a0a73d623c0170e31e557215fc3dca
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-08-21 05:54:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ def init_db():
 @app.route('/<_>', methods=['GET'])
 def home(_=None):
     try:
-        confessions = sqlite3.connect("database.db")
-        cursor = confessions.cursor() 
+        xss = sqlite3.connect("database.db")
+        cursor = xss.cursor() 
         cursor.execute('SELECT * FROM xss') 
         data = cursor.fetchall()
-        confessions.commit()
+        xss.commit()
     except sqlite3.Error as e:
         print(f"\033[91mAn error occurred:\033[00m {e} ")
         init_db()
     finally:
-        if confessions:
-            confessions.close()
+        if xss:
+            xss.close()
     return render_template('index.html', data=data)
 
 
@@ -37,16 +37,16 @@ def Post_Confession():
     if request.method == 'POST': 
         injection = request.form['injection'] 
         try:
-            confessions = sqlite3.connect("database.db")
-            cursor = confessions.cursor()
+            xss = sqlite3.connect("database.db")
+            cursor = xss.cursor()
             cursor.execute("INSERT INTO xss (injection) VALUES (?)", (injection, ) )
-            confessions.commit()
+            xss.commit()
         except sqlite3.Error as e:
             print(f"\033[91mAn error occurred:\033[00m {e} ")
             init_db()
         finally:
-            if confessions:
-                confessions.close()
+            if xss:
+                xss.close()
         return redirect("/", code=302)
     else:
         return render_template('inject.html')
@@ -54,4 +54,4 @@ def Post_Confession():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80)
